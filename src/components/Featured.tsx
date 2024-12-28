213cc9cd0dfdd5ac75bf3c991750f8e345b8e791
Anuj Dubey <anujd2408@gmail.com>
2024-12-29 02:24:52 +0530

    Added

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div>
+        <div>
+            <div>
+                <div>
+                    <Image src="/ddf.jpeg" alt='' fill className='object-contain' ></Image>
+                </div>
+                <div>
+                    <h1></h1>
+                    <p></p>
+                    <span></span>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
