8e7aa76fea35f31d7b321838015e08028f27ff0d
Anuj Dubey <anujd2408@gmail.com>
2024-12-29 01:13:11 +0530

    Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Slider from "@/components/Slider";
+import Featured from "@/components/Featured";
 
 export default function Home() {
   return (
-    <div>hi dost!</div>
+    <main>
+      <Slider></Slider>
+      <Featured></Featured>
+    </main>
   )
 }
