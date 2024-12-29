1be7e34ca99071ad5f05157a0bfc0d2d2d4f762c
Anuj Dubey <anujd2408@gmail.com>
2024-12-29 23:23:18 +0530

    Added

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import Image from 'next/image'
+import { featuredProducts } from '@/data'
 
 const Featured = () => {
   return (
     <div className='w-screen overflow-x-scroll text-gray-600'>
         <div className='w-max flex'>
-            <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
-                <div className='relative flex-1 w-full'>
-                    <Image src="/ddf.jpeg" alt='' fill className='object-contain' ></Image>
+            {/* single product */}
+            {featuredProducts.map((item) => (
+                <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
+
+                 {/* Image */}
+                 {item.img && (
+                    <div className='relative flex-1 w-full'>
+                    <Image src={item.img} alt='' fill className='object-contain' ></Image>
                 </div>
-                <div className='flex-1 flex flex-col gap-4'>
-                    <h1 className='text-xl font-bold uppercase'>Title</h1>
-                    <p>Desc</p>
-                    <span className='text-xl font-bold'>₹25,000</span>
+                 )}
+                
+                {/* Text */}
+                <div className='flex-1 flex flex-col items-center justify-center gap-4'>
+                    <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl'>{item.title}</h1>
+                    <p className='p-4 2xl:p-8'>{item.desc}</p>
+                    <span className='text-xl font-bold'>{item.price}</span>
                     <button className='bg-gray-600 text-white p-2 rounded-md'>Add to Cart</button>
                 </div>
             </div>
+            ))}
+            
         </div>
     </div>
   )
