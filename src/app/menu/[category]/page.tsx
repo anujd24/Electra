a5a2188a1da4ab3d7285e86c87969516ccf718ba
Anuj Dubey <anujd2408@gmail.com>
2024-12-30 14:00:38 +0530

    Added

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { laptops } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const category = () => {
   return (
-    <div>category page</div>
+    <div className='flex flex-wrap text-gray-600'>
+      {laptops.map((item) => (
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-gray-600 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50' href={`/${item.id}`}>
+          {item.img && (
+            <div className='relative h-[80%]'>
+              <Image src={item.img} alt='' fill className='object-contain'></Image>
+            </div>
+          )}
+
+          <div className='flex items-center justify-between font-bold'>
+            <h1 className='text-2xl uppercase p-2'>{item.title}</h1>
+            <h2 className='group-hover:hidden text-xl'>${item.price}</h2>
+            <button className='hidden group-hover:block uppercase bg-gray-600 text-white p-2 rounded-md'>Add to Cart</button>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- src/data.ts
+++ src/data.ts
@@ -114,6 +114,111 @@ export const featuredProducts: Products = [
     }
 ]
 
+export const laptops: Products = [
+    {
+        id: 1,
+        title: "HP 15 ",
+        desc:"Intel Core i5 13th Gen (16GB Ram/512GB SSD/Fhd/Windows 11/Ms Office/Backlit Kb/15.6 Fd0316Tu Laptop ",
+        img:"/hpLast2.jpg",
+        price:35000,
+        options:[
+            {
+                title:"black",
+                additionalPrice:0,
+            },
+            {
+                title:"grey",
+                additionalPrice:0
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "HP Laptop 255 G9",
+        desc:"AMD Athlon Silver 3050U - (8GB/256GB SSD/AMD Radeon Graphics/Windows 11) Thin and Light Business Laptop",
+        img:"/hpLast.jpg",
+        price:39000,
+        options:[
+            {
+                title:"black",
+                additionalPrice:0,
+            },
+            {
+                title:"grey",
+                additionalPrice:0
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Dell Inspiron 15 3535 ",
+        desc:"Dell Inspiron 15 3535 Laptop – 15.6 inch FHD (39.62cm) Display, AMD Ryzen 3 7320U Processor, 8 GB RAM, 512 GB SSD, Carbon Black, 1.67 Kg, Lightweight and Portable",
+        img:"/dellInsp.jpg",
+        price:23000,
+        options:[
+            {
+                title:"black",
+                additionalPrice:0,
+            },
+            {
+                title:"grey",
+                additionalPrice:1500
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Lenovo V15 G4 ",
+        desc:"8GB LPDDR5 Ram, 512 GB SSD PCIe, Windows 11 Lifetime Validity",
+        img:"/lenovo.jpg",
+        price:72900,
+        options:[
+            {
+                title:"black",
+                additionalPrice:0,
+            },
+            {
+                title:"Athlon Silver",
+                additionalPrice:2000,
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Dell Vostro 14 3420 Laptop",
+        desc:"Dell Vostro 14 3420 Laptop – 14 inch FHD (35.56cm) Display, 12th Gen Intel Core i5-1235U Processor, 8 GB RAM, 512 GB SSD",
+        img:"/dellvos.avif",
+        price:47990,
+        options:[
+            {
+                title:"black",
+                additionalPrice:1200,
+            },
+            {
+                title:"titan grey",
+                additionalPrice:2300
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Acer Chromebook ",
+        desc:"CB315-4H Intel Celeron N4500 (Chrome OS/8 GB RAM/128 GB/WiFi 6) 39.6 cm ",
+        img:"/acer.jpg",
+        price:25090,
+        options:[
+            {
+                title:"black",
+                additionalPrice:1000,
+            },
+            {
+                title:"titan grey",
+                additionalPrice:2000
+            }
+        ]
+    }
+]
+
 type Menu = {
     id:number,
     slug:string,
@@ -149,3 +254,4 @@ export const menu: Menu = [
         color:"Blue"
     },
 ]    
+
