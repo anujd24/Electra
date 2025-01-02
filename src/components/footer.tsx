7b981795d50df5762cd63ee3a8022d1fd2607d17
Anuj Dubey <anujd2408@gmail.com>
2025-01-02 23:48:07 +0530

    Added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-gray-600 flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-gray-600 flex items-center justify-between'>
         <Link href="/" className='font-bold text-xl'>ELECTRA</Link>
         <p>® ALL RIGHTS RESERVED.</p>
     </div>
