e2620e2c8ba445da22d330cba69699f7f31004d1
Anuj Dubey <anujd2408@gmail.com>
2025-01-04 12:30:00 +0530

    Added

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+
+import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+
 
 export const GET = async() => {
     try{
