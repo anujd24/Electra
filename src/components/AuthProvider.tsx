f6a30739b4d1e733ca3c244f09e2bbaef7422586
Anuj Dubey <anujd2408@gmail.com>
2025-01-12 20:34:08 +0530

    adding

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,1 +1,12 @@
-"use client"
+"use client";
+import { SessionProvider } from "next-auth/react"
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AuthProvider = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
