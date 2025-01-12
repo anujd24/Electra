dc70640cb432df0cc17bb6d5187123f7f98b36ab
Anuj Dubey <anujd2408@gmail.com>
2025-01-12 20:37:25 +0530

    adding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Notification from "@/components/notification";
 import Navbar from "@/components/navbar";
+import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
+import AuthProvider from "@/components/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Electra",
@@ -22,18 +17,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Notification/>
-        <Navbar/>
-        {children}
-        <Footer/>
+      <body className={inter.className}>
+        <AuthProvider>
+            <div>
+              <Notification />
+              <Navbar />
+              {children}
+              <Footer />
+              
+            </div>
+          
+        </AuthProvider>
       </body>
     </html>
   );
