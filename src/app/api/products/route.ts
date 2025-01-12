890efe799987b6ca1c4ca195f45cd33eaa704443
Anuj Dubey <anujd2408@gmail.com>
2025-01-12 19:00:09 +0530

    adding

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,12 +1,19 @@
 
 import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
 
-export const GET = async() => {
-    try{
-        const products = await prisma.product.findMany();
+export const GET = async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url);
+    const cat = searchParams.get("cat");
+  
+    try {
+      const products = await prisma.product.findMany({
+        where: {
+          ...(cat ? { catSlug: cat } : { isFeatured: true }),
+        },
+      });
         return new NextResponse(JSON.stringify(products), {status:200});
     }catch(err){
         console.log(err);
