d483a5e8a2ad87a806e00be4626f95c845baf538
Anuj Dubey <anujd2408@gmail.com>
2025-01-13 01:52:30 +0530

    adding

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 
 const OrdersPage = () => {
+
+  
+
   return (
     <div className="p-4 lg:px-20 xl:px-40">
       <table className="w-full border-separate border-spacing-3">
