725b0476454f31e7485e5034fe9b79ffb4eb86c3
Anuj Dubey <anujd2408@gmail.com>
2025-01-14 01:35:34 +0530

    adding

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -28,7 +28,7 @@ const Featured = async () => {
 
                  {/* Image */}
                  {item.img && (
-                    <div className='relative flex-1 w-full'>
+                    <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
                     <Image src={item.img} alt='' fill className='object-contain' ></Image>
                 </div>
                  )}
