d20bd721a04105be377e0e90fbac79e31cd520c4
Anuj Dubey <anujd2408@gmail.com>
2025-01-14 01:07:00 +0530

    adding

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -16,7 +16,10 @@ const getData = async (id: string) => {
   return res.json();
 };
 
-const SingleProductPage = () => {
+const SingleProductPage = async ({ params }: { params: { id: string } }) => {
+
+  
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-gray-500 md:flex-row md:gap-8 md:items-center">
       {/* IMAGE CONTAINER */}
