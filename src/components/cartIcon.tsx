cab3a02cc09f9bde505785c5d8e97f6432757bd9
Anuj Dubey <anujd2408@gmail.com>
2025-01-14 01:31:56 +0530

    adding

--- src/components/cartIcon.tsx
+++ src/components/cartIcon.tsx
@@ -6,6 +6,9 @@ import React, {useEffect} from 'react'
 import Image from 'next/image'
 
 const CartIcon = () => {
+
+  
+
   return (
     <Link href="/cart" className='flex items-center gap-4'>
         <div className='relative w-8 h-8 md:w-5 md:h-5'>
