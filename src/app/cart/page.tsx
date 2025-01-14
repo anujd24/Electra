515f4d64eda700f2bdf0a5655fe6841fcc211ad5
Anuj Dubey <anujd2408@gmail.com>
2025-01-15 01:15:06 +0530

    adding

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -66,31 +66,33 @@ const CartPage = () => {
         ))}
       </div>
     {/* payment */}
-    <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6'> 
-      <div className=' flex justify-between'>
-        <span className=''>Subtotal (2 items)</span>
-        <span className=''> 90,000</span>
+    <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className="flex justify-between">
+          <span className="">Subtotal ({totalItems} items)</span>
+          <span className="">₹{totalPrice}</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="">Service Cost</span>
+          <span className="">₹0.00</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="">Delivery Cost</span>
+          <span className="text-green-500">FREE!</span>
+        </div>
+        <hr className="my-2" />
+        <div className="flex justify-between">
+          <span className="">TOTAL(INCL. VAT)</span>
+          <span className="font-bold">₹{totalPrice}</span>
+        </div>
+        <button
+          className="bg-gray-500 text-white p-3 rounded-md w-1/2 self-end"
+          onClick={handleCheckout}
+        >
+          CHECKOUT
+        </button>
       </div>
-
-      <div className=' flex justify-between'>
-        <span className=''>Service Cost</span>
-        <span className=''> ₹0</span>
-      </div>
-
-      <div className=' flex justify-between'>
-        <span className=''>Delivery Cost</span>
-        <span className='text-green-500'> FREE!</span>
-      </div>
-      <hr />
-
-      <div className=' flex justify-between'>
-        <span className=''>TOTAL(INCL. VAT)</span>
-        <span className='font-bold'> ₹5,000</span>
-      </div>
-      <button className='bg-gray-600 text-white p-3 rounded-md w-1/2'>CHECKOUT</button>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default CartPage
